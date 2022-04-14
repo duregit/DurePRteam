408dc7f9a299fafb27cde166f153cfa014256111
in970817 <in970817@naver.com>
2022-04-14 09:38:16 +0900

    마이페이지 쪽 로그인 세션 유지 및 주소 API 적용

--- DurePRteam/src/main/java/com/example/DurePRteam/controller/MyController.java
+++ DurePRteam/src/main/java/com/example/DurePRteam/controller/MyController.java
@@ -1,16 +1,73 @@
 package com.example.DurePRteam.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.DurePRteam.dto.My;
+import com.example.DurePRteam.mapper.BCodeMapper;
+import com.example.DurePRteam.mapper.MyMapper;
 
 @Controller
+@RequestMapping("my")
 public class MyController {
 
-    @RequestMapping("/my/my01")
-    public String my01() throws Exception {
+	@Autowired MyMapper myMapper;
+    @Autowired BCodeMapper bcodeMapper;
+
+
+    @RequestMapping("my01")
+    //@ResponseBody
+    public String my01(HttpSession session , Model model) throws Exception {
+    	//System.out.println(session.getAttribute("login_id"));
+    	String login_id = (String) session.getAttribute("login_id");
+
+    	My my = myMapper.findUser(login_id);
+    	System.out.println(myMapper.findUser(login_id));
+
+    	model.addAttribute("my", my);
+
+		model.addAttribute("bCode", bcodeMapper.findBCAll()); // 은행
+    	//System.out.println(bcodeMapper.findBCAll());
+
 
         return "my/my01";
+    }
+
+
+    // 마이페이지 수정
+	@PostMapping("my01")
+	public String update(HttpSession session , Model model , My my) {
+    	String login_id = (String) session.getAttribute("login_id");
+		System.out.println(my.getUserPW());
+		System.out.println(login_id);
+		myMapper.update(my);
+
+		return "redirect:/my/my01";
+	}
+
+
+	// 비밀번호 변경
+	@RequestMapping("updatePW")
+	@ResponseBody
+    public String updatePW(HttpSession session , HttpServletResponse response, @RequestBody My my ) throws Exception {
+    	String login_id = (String) session.getAttribute("login_id");
+		System.out.println(my);
+		System.out.println(my.getUserPW());
+		//int idChkCount = myMapper.findId(jn.getUserId());
+		myMapper.pwUpdate(my);
+
+		//System.out.println(count);
+
+		return null;
+
     }
 
 }
---
+++ DurePRteam/src/main/java/com/example/DurePRteam/dto/My.java
@@ -0,0 +1,20 @@
+package com.example.DurePRteam.dto;
+
+import lombok.Data;
+
+@Data
+public class My {
+	String userId;
+	String userPW;
+	String userName ;
+	String piProperty;
+	String piPropName;
+	String suPIProperty;
+	String suPIPropName;
+	String userZip ;
+	String userAddr1 ;
+	String userAddr2 ;
+	String userCtel ;
+	String bCode;
+	String userACNum;
+}
